f48e8bea6305bea61795527db4dc28707f093044
AidanS39 <aidan.stoner39@gmail.com>
2024-11-18 23:07:40 -0500

    added green drop shadow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { useState } from 'react'
 const Paragraph = ({ text, title, direction}) => {
   return (
     <div className={`flex flex-col ${direction} `}>
-      <h3 className={`w-1/2 antialiased font-space pt-8 pb-4 px-4 text-white text-3xl`}>{title}</h3>
-      <p className={`w-1/2 font-space text-white text-left px-4`}>{text}</p>
+      <h3 className={`w-1/2 antialiased font-space pt-8 pb-4 px-4 text-white text-3xl drop-shadow-green-md`}>{title}</h3>
+      <p className={`w-1/2 font-space text-white text-left hyphens-auto px-4`}>{text}</p>
     </div>
   )
 }
@@ -13,12 +13,12 @@ const Header = () => {
   return (
     <div>
       <div>
-        <h1 className='antialiased font-space pt-8 pb-4 text-white text-center text-6xl select-none'>Aidan Stoner</h1>
+        <h1 className='antialiased font-space pt-8 pb-4 text-white text-center text-6xl drop-shadow-green-lg select-none'>Aidan Stoner</h1>
       </div>
       <div className='flex gap-8'>
-        <a href='https://www.linkedin.com/in/aidanstoner' className='flex-auto text-white text-right font-space hover:underline'>LinkedIn</a>
-        <a href='https://github.com/AidanS39' className='flex-4 text-white text-center font-space hover:underline'>GitHub</a>
-        <a href='https://www.linkedin.com/in/aidanstoner' className='flex-auto text-white text-left font-space hover:underline'>LinkedIn</a>
+        <a href='https://www.linkedin.com/in/aidanstoner' className='flex-auto text-white text-right font-space hover:drop-shadow-green-sm underline'>LinkedIn</a>
+        <a href='https://github.com/AidanS39' className='flex-4 text-white text-center font-space hover:drop-shadow-green-sm underline'>GitHub</a>
+        <a href='https://www.linkedin.com/in/aidanstoner' className='flex-auto text-white text-left font-space hover:drop-shadow-green-sm underline'>LinkedIn</a>
       </div>
     </div>
   )
@@ -26,11 +26,11 @@ const Header = () => {
 
 function App() {
 
-  const introParagraph="My name is Aidan Stoner and I am a sophomore majoring in Computer Science at The College of New Jersey. This website acts as both a portfolio and a resume, where you can see my personal work and experience."
+  const introParagraph="My name is Aidan Stoner and I am a sophomore majoring in Computer Science at The College of New Jersey. This website acts as both a portfolio and a resume, where you can see my personal work and experience. Everything you see on this website was developed by myself. The reason why I created the entire site myself was to gain experience in full-stack development."
 
   return (
-    <div className='min-h-screen p-2 bg-green-950'>
-      <div className='p-5 m-2 bg-zinc-900 border-4 rounded-3xl'>
+    <div className='min-h-screen p-2 bg-green-900 shadow-inner-xl'>
+      <div className='p-5 m-2 bg-zinc-800 border-4 rounded-xl'>
         <Header />
         <Paragraph title='Introduction' text={introParagraph} direction='items-start' />
       </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,11 +7,14 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        'inner-xl': 'inset 0 50px 100px 0 rgb(0 0 0 / 0.65)'
+        'inner-xl': 'inset 0px 0px 40px 0 rgba(0, 0, 0, 1.00)'
       },
       dropShadow: {
-        'normal': '0 10px 10px rgb(0 0 0 / 0.60)',
-        '3xl': '0 10px 20px rgb(0 0 0 / 0.60)'
+        'normal': '0 10px 20px rgba(0, 0, 0, 0.60)',
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.60)',
+        'green-lg': '2px 4px 0px rgba(0, 100, 0, 1.00)',
+        'green-sm': '1px 2px 0px rgba(0, 100, 0, 1.00)',
+        'green-md': '1.5px 3px 0px rgba(0, 100, 0, 1.00)',
       },
       fontFamily:{
         space: ["Space Mono", "sans-serif"]
