c2e0b92fc4a648b6212d9e72f2e658e3b6fd8cbd
AidanS39 <aidan.stoner39@gmail.com>
2024-11-21 02:37:56 -0500

    finished certifications, improved dynamic organization of components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,53 +5,53 @@ import certificationsServices from './services/certifications'
 
 const Subtitle = ({ title }) => {
   return (
-    <h1 className={`antialiased font-space pt-8 pb-1 px-4 text-white tracking-wide text-3xl drop-shadow-green-md`}>{title}</h1>
+    <h1 className={`antialiased font-space pt-4 pb-1 text-white tracking-wide text-3xl drop-shadow-green-md`}>{title}</h1>
   )
 }
 
 const Header = ({ title }) => {
   return (
-    <h2 className={`antialiased font-space py-2 pb-1 px-4 text-white text-xl underline`}>{title}</h2>
+    <h2 className={`antialiased font-space py-2 text-white text-xl tracking-tighter underline`}>{title}</h2>
   )
 }
 
 const SubHeader = ({ title }) => {
   return (
-    <h2 className={`antialiased font-space px-4 text-white text-base`}>{title}</h2>
+    <h2 className={`antialiased font-space py-1 text-white text-base`}>{title}</h2>
   )
 }
 
 
 const Paragraph = ({ text, width }) => {
   return (
-    <p className={`${width} font-space text-white text-left text-sm tracking-tight px-4 py-1`}>{text}</p>
+    <p className={`${width} font-space text-white text-left text-sm tracking-tight py-1`}>{text}</p>
   )
 }
 
-const HeaderParagraph = ({ direction, width, title, text }) => {
+const HeaderParagraph = ({ title, text }) => {
   
   return (
-    <div className={`flex flex-col ${direction}`}>
+    <div className='flex flex-col'>
       <Subtitle title={title} />
-      <Paragraph text={text} width={width} />
+      <Paragraph text={text} />
     </div>
   )
 }
 
-const Experience = ( experience ) => {
-  const currentExperience = experience.experience
-  const endDate = currentExperience.stillWorking ? "Current" : currentExperience.endDate
+const Experience = ({ experience }) => {
+  const endDate = experience.stillWorking ? "Current" : experience.endDate
   
   let skills = ""
-  currentExperience.skills.forEach(skill => skills = skills.concat(`${skill} • `))
+  experience.skills.forEach(skill => skills = skills.concat(`${skill} • `))
   skills = skills.substring(0, skills.length - 3)
 
   return (
     <div>
-      <Header title={currentExperience.title} />
-      <SubHeader title={currentExperience.location} />
-      <SubHeader title={`${currentExperience.startDate} - ${endDate}`} />
-      <Paragraph text={currentExperience.description} />
+      <Header title={`${experience.title}`} />
+      <SubHeader title={` ${experience.employer}`} />
+      <SubHeader title={experience.location} />
+      <SubHeader title={`${experience.startDate} - ${endDate}`} />
+      <Paragraph text={experience.description} />
       <Paragraph text={skills} />
     </div>
   )
@@ -70,9 +70,9 @@ const Experiences = ({ title, experiences }) => {
 
 const Certification = ({ certification }) => {
   return (
-    <div>
+    <div className='flex py-2 items-center'>
       <div>
-      <img src={certification.image} href={certification.link} />
+      <a href={certification.link}><img src={certification.image} href={certification.link} className='px-2 cursor-pointer h-[168px]' /></a>
       </div>
       <div>
         <Header title={certification.name} />
@@ -100,9 +100,13 @@ const Title = () => {
       <div>
         <h1 className='antialiased font-space pt-8 pb-4 text-white text-center text-6xl drop-shadow-green-lg select-none'>Aidan Stoner</h1>
       </div>
-      <div className='flex gap-8'>
-        <a href='https://www.linkedin.com/in/aidanstoner' className='flex-auto text-white text-right font-space px-4 hover:drop-shadow-green-sm underline'>LinkedIn</a>
-        <a href='https://github.com/AidanS39' className='flex-auto text-white text-left font-space px-4 hover:drop-shadow-green-sm underline'>GitHub</a>
+      <div className='flex gap-8 pb-8'>
+        <div className='flex-auto text-right'>
+          <a href='https://www.linkedin.com/in/aidanstoner' className='text-white font-space text-xl px-4 hover:drop-shadow-green-sm underline'>LinkedIn</a>
+        </div>
+        <div className='flex-auto text-left'>
+          <a href='https://github.com/AidanS39' className='text-white font-space text-xl px-4 hover:drop-shadow-green-sm underline'>GitHub</a>
+        </div>
       </div>
     </div>
   )
@@ -138,11 +142,14 @@ function App() {
 
   return (
     <div className='min-h-screen p-2 bg-green-900'>
-      <div className='p-5 m-2 bg-zinc-800 border-4 rounded-xl shadow-3xl'>
+      <div className='p-12 m-6 bg-zinc-800 border-4 rounded-xl shadow-3xl'>
         <Title />
-        <HeaderParagraph title={introduction.name} text={introduction.text} direction='items-start' width="w-3/5"/>
-        <Experiences title="Work Experience" experiences={experiences} />
-        <Certifications title="Certifications" certifications={certifications} />
+        <div className='grid md:grid-cols-1 xl:grid-cols-2 gap-2'>
+          <div className='row-auto'><HeaderParagraph title={introduction.name} text={introduction.text} direction='items-start' width="w-3/5"/></div>
+          <div className='row-span-3'><Experiences title="Work Experience" experiences={experiences} /></div>
+          {/* TO DO: EDUCATION */}
+          <div className='row-auto'><Certifications title="Certifications" certifications={certifications} /></div>
+        </div>
       </div>
     </div>
   )
